5fd6e07bd89f5a6535ec03833c2ec1c257745590
Archer <sp38013@naver.com>
2023-03-24 17:37:37 +0900

    webpack 설정 및 Typescript loader 셋팅[sy.namgoong]
    1.webpack 설정
    2.Typescript loader 셋팅
    3.package.json 추가 및 script 수정

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log('test ');
+
+// "complie": "tsc",
+//     "build": "parcel build src/index.html",
+//     "dev": "parcel src/index.html --open",
+//     "start": "npm run complie&&npm run build && npm run dev"
